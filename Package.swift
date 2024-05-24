1b40e410e020814fdb21af90ccabe95b4fa02494
Alex little <alex@reelapp.co.uk>
2024-05-24 21:49:38 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-perception", exact: "1.1.7")
+        .package(url: "https://github.com/pointfreeco/swift-perception", exact: "1.1.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
