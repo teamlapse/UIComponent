e04b97c5c0481b4cd27f0c092faebad883fbf55d
Alex Little <alex@reelapp.co.uk>
2025-02-01 19:10:47 +0000

    fix: size warnings

--- Sources/UIComponent/Components/View/ObservationBoundaryComponent.swift
+++ Sources/UIComponent/Components/View/ObservationBoundaryComponent.swift
@@ -1,7 +1,7 @@
 //  Created by Alex Little on 01/02/2025.
 
-import UIKit
 import IssueReporting
+import UIKit
 
 /// Protects the ancestor or sibling views from reloading when model properties change that are accessed in this subheirarchy
 /// This can isolate heavy model updates to a single component without reloading an entire screen
@@ -9,24 +9,27 @@ public struct ObservationBoundaryComponent<C: ComponentBuilder>: ComponentBuilde
     let componentBuilder: C
 
     public init(component: C) {
-        self.componentBuilder = component
+        componentBuilder = component
     }
 
     public func build() -> some Component {
         ViewComponent<UIView>()
             .with(\.componentEngine.component, componentBuilder)
-#if DEBUG
+        #if DEBUG
             .constraint { c in
-                if c.maxSize.height == .infinity || c.minSize.height == 0 {
-                    reportIssue("You must provide a height for \(String(describing: componentBuilder)) in a ObservationBoundaryComponent's")
-                }
+                let hasInvalidHeight = (c.maxSize.height == .infinity && c.minSize.height == 0) ||
+                    (c.maxSize.height > 0 && c.minSize.height == 0)
+                let hasInvalidWidth = (c.maxSize.width == .infinity && c.minSize.width == 0) ||
+                    (c.maxSize.width > 0 && c.minSize.width == 0)
 
-                if c.maxSize.width == .infinity || c.minSize.width == 0 {
-                    reportIssue("You must provide a width for \(String(describing: componentBuilder)) in a ObservationBoundaryComponent's")
+                if hasInvalidHeight, hasInvalidWidth {
+                    withIssueReporters([.runtimeWarning, .breakpoint]) {
+                        reportIssue("View cannot layout - dimensions are not properly constrained (missing minimum size) for \(String(describing: componentBuilder)) in ObservationBoundaryComponent")
+                    }
                 }
                 return c
             }
-#endif
+        #endif
             .eraseToAnyComponentOfView()
     }
 }
@@ -37,25 +40,27 @@ public struct ObservationScrollBoundaryComponent<C: ComponentBuilder>: Component
     let componentBuilder: C
 
     public init(component: C) {
-        self.componentBuilder = component
+        componentBuilder = component
     }
-
 
     public func build() -> AnyComponentOfView<UIScrollView> {
         ViewComponent<UIScrollView>()
             .with(\.componentEngine.component, componentBuilder)
-#if DEBUG
+        #if DEBUG
             .constraint { c in
-                if c.maxSize.height == .infinity || c.minSize.height == 0 {
-                    reportIssue("You must provide a height for \(String(describing: componentBuilder)) in a ObservationScrollBoundaryComponent's")
-                }
+                let hasInvalidHeight = (c.maxSize.height == .infinity && c.minSize.height == 0) ||
+                    (c.maxSize.height > 0 && c.minSize.height == 0)
+                let hasInvalidWidth = (c.maxSize.width == .infinity && c.minSize.width == 0) ||
+                    (c.maxSize.width > 0 && c.minSize.width == 0)
 
-                if c.maxSize.width == .infinity || c.minSize.width == 0 {
-                    reportIssue("You must provide a width for \(String(describing: componentBuilder)) in a ObservationScrollBoundaryComponent's")
+                if hasInvalidHeight, hasInvalidWidth {
+                    withIssueReporters([.runtimeWarning, .breakpoint]) {
+                        reportIssue("View cannot layout - dimensions are not properly constrained (missing minimum size) for \(String(describing: componentBuilder)) in ObservationScrollBoundaryComponent")
+                    }
                 }
                 return c
             }
-#endif
+        #endif
             .eraseToAnyComponentOfView()
     }
 }
