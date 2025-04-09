38ca2e8b128d75f9473d421c71ac7b9d779b0313
Jacob Bartlett <jacob@lapse.com>
2025-04-09 15:15:56 +0100

    fix: Commit for infinite loop testing

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.0.0"),
-        .package(url: "https://github.com/teamlapse/swift-perception", revision: "b5500c32b22ea461cb1a2298f94a6ad6c93a4df2"),
+        .package(url: "https://github.com/teamlapse/swift-perception", revision: "cb958770729e352d709927c29fd5b9a28c89594b"),
         .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", from: "1.0.0"),
     ],
     targets: [
